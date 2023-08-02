d5d06d4086dad8161affabc79fdfba565e82e54f
kennydiaz1 <kennydiaz3@gmail.com>
2023-08-02 16:01:52 -0500

    prueba

---
+++ main.js
@@ -0,0 +1,14 @@
+//ARRAY.push() method
+const fruits = ["Apple", "Peach"];
+console.log(fruits.push("Banana"));
+console.log(fruits);
+
+//ARRAY.pop() method
+const fruits1 = ["Apple", "Peach"];
+fruits.pop();
+console.log(fruits);
+
+//ARRAY.shift() method
+const fruits2 = ["Apple", "Peach"];
+fruits.shift();
+console.log(fruits);
