316e0e2786061cef7c49184ace49e18d886e4351
kennydiaz1 <kennydiaz3@gmail.com>
2023-10-04 16:39:20 -0500

    Corrigiendo

--- Clase1_React_Vite/src/main.jsx
+++ Clase1_React_Vite/src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import GifExpertApp  from './componentApp'
-import Giftgrid from './components/GifGrid'
+
 
 
 
@@ -13,7 +12,5 @@ import Giftgrid from './components/GifGrid'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <GifExpertApp title = 'Gift App' />
-    <Giftgrid title = 'Challenge - 12 ' />
   </React.StrictMode>,
 )
