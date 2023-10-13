45a7866323b431848aba959a7c5e00add8982c21
kennydiaz1 <kennydiaz3@gmail.com>
2023-10-12 20:32:15 -0500

    Challenge-13

---
+++ Clase1_React_Vite/src/FirstApp.jsx
@@ -0,0 +1,16 @@
+import useCounter from "./hooks/useCounter";
+
+function FirstApp() {
+  const { count, increment, decrement, reset } = useCounter(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default FirstApp;
